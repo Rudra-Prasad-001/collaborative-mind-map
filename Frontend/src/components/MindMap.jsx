4624121670051f277b8986297b85e8ff7f3578fe
Rudra Prasad Pati <rudraprasadpati478@gmail.com>
2025-06-15 01:35:13 +0530

    installed react flow and created a basic react flow component

---
+++ Frontend/src/components/MindMap.jsx
@@ -0,0 +1,13 @@
+import Reactflow from 'reactflow'
+import 'reactflow/dist/style.css'
+
+const MindMap = () => {
+    return (
+        <div style={{width : '100vw', height: '100vh'}}>
+        <Reactflow />
+        </div>
+    )
+}
+
+
+export default MindMap
