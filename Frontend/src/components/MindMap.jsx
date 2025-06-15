7b69d5c26b6fbad1e8578bed6bf3226774416064
Rudra Prasad Pati <rudraprasadpati478@gmail.com>
2025-06-16 03:34:59 +0530

    added interactivity to my graph (handels selecting/deselcting/deleting etc of nodes and edges)

--- Frontend/src/components/MindMap.jsx
+++ Frontend/src/components/MindMap.jsx
@@ -1,11 +1,66 @@
-import { ReactFlow, Background, Controls } from '@xyflow/react';
+import { useCallback, useState } from 'react';
+import { 
+    ReactFlow,
+    Background,
+    Controls,
+    applyEdgeChanges,
+    applyNodeChanges
+} from '@xyflow/react';
 //Without importing this style.css the react flow will not work
 import '@xyflow/react/dist/style.css';
 
+const initialNodes = [
+  {
+    id: '1',
+    position: {x: 0, y: 0},
+    data: {label: 'First node'}
+    
+ },
+ {
+    id: '2',
+    position: {x:100, y: 110},
+    data: {label: 'second node'}
+ }
+]
+
+const initialEdges = [
+    {
+        id: '1-2',
+        source: '1',
+        target: '2',
+        label: 'Edge'
+    }
+]
+
 export default function MindMap() {
+    const [nodes, setNodes] = useState(initialNodes)
+    const [edges, setEdges] = useState(initialEdges)
+
+    //useCallback optimize the performance by not recreating the fn in every renders (it returns the fn not return value)
+    
+    const onNodesChange = useCallback(
+        (userActions) => {
+            setNodes(
+                (nds) => applyNodeChanges(userActions,nds)
+            )
+        }, []
+    )
+    const onEdgesChange = useCallback(
+        (userActions) => {
+            setEdges(
+                (eds) => applyEdgeChanges(userActions, eds)
+            )
+        }, []
+    )
     return(
         <div style={{height :'100vh'}}>
-            <ReactFlow>
+            <ReactFlow
+            nodes={nodes} 
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            fitView
+            >
                 <Background />
                 <Controls />
             </ReactFlow>
